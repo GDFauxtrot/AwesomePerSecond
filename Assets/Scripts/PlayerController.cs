15b742529807bc3f514c15041f5c76349a6a72d3
Greg Duarte <gregduarte97@gmail.com>
2018-08-18 09:39:58 -0700

    Ground Pound & physics fun
      + Finished ground pound
      + Added controller support
      + Created physics objects in SampleScene to blow up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour {
     Vector2 mouseInput, moveInput;
     Vector2 mouseRotation;
 
+    bool grounded;
+
     [Header("Camera")]
     public Vector2 cameraDistanceMinMax;
     public float cameraDistanceStart;
@@ -24,11 +26,18 @@ public class PlayerController : MonoBehaviour {
     float cameraDistance;
     Camera camera;
 
+    [Header("Ground Pound")]
+
+    public float groundPoundSpeed;
+    public float groundPoundForce;
+    public float groundPoundAirLift;
+    public float groundPoundRadius;
+    bool groundPounding;
+
     [Header("Misc")]
     public Rigidbody rb;
     public SphereCollider sphere;
-    public bool drawDebugLines;
-    bool grounded;
+    public bool drawDebug;
 
     Collider[] overlapCols; // using nonalloc
 
@@ -46,10 +55,13 @@ public class PlayerController : MonoBehaviour {
 
     void Update() {
         // Gather inputs
-        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseInput = new Vector2(
+            Input.GetAxisRaw("Mouse X") + Input.GetAxisRaw("Joy X"),
+            -Input.GetAxisRaw("Mouse Y") + Input.GetAxisRaw("Joy Y"));
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         mouseRotation += mouseInput * cameraSensitivity;
         bool jumpPressed = Input.GetButtonDown("Jump");
+        bool groundPoundPressed = Input.GetButtonDown("Ground Pound");
 
         #region CAMERA
 
@@ -58,7 +70,7 @@ public class PlayerController : MonoBehaviour {
 
         // Lock rotation at major up/down (so you aren't looking upside-down behind you)
         mouseRotation.y = Mathf.Clamp(mouseRotation.y, -90, 90);
-        cameraTarget.transform.Rotate(-mouseRotation.y, mouseRotation.x, 0);
+        cameraTarget.transform.Rotate(mouseRotation.y, mouseRotation.x, 0);
 
         // Scroll in/out w/ scroll wheel
         cameraDistance += -Input.GetAxisRaw("Mouse ScrollWheel") * cameraDistanceScrollAmount;
@@ -71,7 +83,7 @@ public class PlayerController : MonoBehaviour {
         Vector3 forceForward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z);
         Vector3 forceRight = new Vector3(camera.transform.right.x, 0, camera.transform.right.z);
 
-        if (drawDebugLines) {
+        if (drawDebug) {
             Debug.DrawRay(transform.position, forceForward, Color.red);
             Debug.DrawRay(transform.position, forceRight, Color.blue);
         }
@@ -87,10 +99,20 @@ public class PlayerController : MonoBehaviour {
                 forceForward * moveInput.y * playerSpeed * airControlMultiplier +
                 forceRight * moveInput.x * playerSpeed * airControlMultiplier);
         }
+        
         // Jump impulse
         if (jumpPressed && grounded) {
             grounded = false;
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        // Activate ground pound motion
+        if (groundPoundPressed && !grounded && !groundPounding) {
+            if (-rb.velocity.y < groundPoundSpeed)
+                rb.velocity = Vector3.down * groundPoundSpeed;
+            else
+                rb.velocity = Vector3.down * -rb.velocity.y;
+            groundPounding = true;
         }
 
         #endregion
@@ -106,13 +128,42 @@ public class PlayerController : MonoBehaviour {
             Vector3 colPoint = overlapCols[i].ClosestPoint(transform.position);
             Vector3 colNormal = (transform.position - colPoint).normalized;
 
-            if (drawDebugLines) {
+            if (drawDebug) {
                 Debug.DrawRay(colPoint, colNormal, Color.magenta);
             }
 
             // This amount can be changed later (89.9 degree slopes will techically count as "grounded" in this state)
             if (colNormal.y > 0)
                 grounded = true;
+        }
+
+        // Ground pound impact
+        if (grounded && groundPounding) {
+            groundPounding = false;
+
+            Collider[] poundedObjects = Physics.OverlapSphere(transform.position, groundPoundRadius, 1 << LayerMask.NameToLayer("GroundPoundable"));
+            foreach (Collider obj in poundedObjects) {
+
+                // Make sure the object isn't obscured by another one (floor/wall/ceiling/etc)
+                bool objectVisible = true;
+                Vector3 ray = obj.transform.position - transform.position;
+                RaycastHit[] hits = Physics.RaycastAll(transform.position, ray.normalized, ray.magnitude);
+                foreach (RaycastHit hit in hits) {
+                    if (hit.collider.gameObject.layer != LayerMask.NameToLayer("GroundPoundable"))
+                        objectVisible = false;
+                }
+
+                if (objectVisible) {
+                    Rigidbody objRB = obj.gameObject.GetComponent<Rigidbody>();
+                    if (objRB) {
+                        float v = Mathf.Max(-rb.velocity.y, groundPoundSpeed); // sometimes it no fast
+
+                        objRB.AddExplosionForce(v * groundPoundForce, transform.position, groundPoundRadius, groundPoundAirLift);
+                    } else {
+                        Debug.LogWarning("Ground poundable object '" + obj.name + "' has no rigidbody!");
+                    }
+                }
+            }
         }
 
         #endregion
@@ -122,12 +173,28 @@ public class PlayerController : MonoBehaviour {
         RotateCameraToTarget();
     }
 
+    void OnDrawGizmos() {
+        // Ground pound radius
+        if (drawDebug) {
+            Gizmos.color = new Color(1f, 0.5f, 0f, 1f);
+            Gizmos.DrawWireSphere(transform.position, groundPoundRadius);
+        }
+    }
+
     /// <summary>
     /// Sets camera position and the rotation to the target, and then moves backwards the desired distance.
     /// </summary>
     private void RotateCameraToTarget() {
         camera.transform.position = cameraTarget.transform.position;
         camera.transform.rotation = cameraTarget.transform.rotation;
-        camera.transform.position -= camera.transform.forward * cameraDistance;
+
+        RaycastHit hit;
+        
+        if (Physics.Raycast(cameraTarget.transform.position, -camera.transform.forward, out hit, cameraDistance)) {
+            camera.transform.position = hit.point + (camera.transform.forward * 0.1f);
+        } else {
+            camera.transform.position -= camera.transform.forward * cameraDistance;
+        }
+        
     }
 }
